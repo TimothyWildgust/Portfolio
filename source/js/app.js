4de1f6d6d761a7a3a5e5a124b7db2a90ce9bb335
Timmy <timmy@wayworks.ie>
2019-02-20 20:05:40 +0000

    Update 0.1
    Updates to classes
    SASS Clean up
    Main content background
    CSS Triangles

--- source/js/app.js
+++ source/js/app.js
@@ -1,48 +1,13 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  $('#slider').slick({
-    autoplay: false,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 300,
-    centerMode: true,
-    fade: true,
-    cssEase: 'linear',
-    infinite: true
-  });
-
-//scroll for section class="information"
-  (function($) {
-      $("#js-scroll").mCustomScrollbar({
-        theme: "minimal",
-        mouseWheel: {
-          scrollAmount: 50
-        },
-        autoHideScrollbar: false,
-        scrollInertia: 500,
-         scrollEasing:"linear"
-      });
-  })(jQuery);
-
-});
+var el = document.getElementsByClassName("skill_div")[0].getElementsByTagName("div");
+for (i = 0; i <=el.length; i++ -1){
+    el[i].classList.add('dot');
+}
 
 
-if (document.getElementById('js-map')) {
-  function initMap() {
-    var map = new google.maps.Map(document.getElementById('js-map'), {
-      zoom: 15,
-      disableDefaultUI: true,
-      center: {
-        lat: 45.802982,
-        lng: 16.004666
-      }
-    });
 
-    var marker = new google.maps.Marker({
-      position: map.getCenter(),
-      icon: "assets/img/marker_icon.png",
-      map: map
-    });
-  };
-};
+
+
+
+// };
