ad85b408177c62b1faaec754efecde3e0961efa2
Timmy <timmy@wayworks.ie>
2019-02-22 04:54:40 +0000

    Version 0.3
    Col fix
    Nav
    Fix z-index
    Fix Duplicate ID's
    Certificate page mock

--- source/js/app.js
+++ source/js/app.js
@@ -1,12 +1,32 @@
 // $(document).ready(function() {
 
-var el = document.getElementsByClassName("skill_div")[0].getElementsByTagName("div");
-for (i = 0; i <=el.length; i++ -1){
-    el[i].classList.add('dot');
+// var el = document.getElementsByClassName("skill_div")[0].getElementsByTagName("div");
+// for (i = 0; i <=el.length; i++ -1){
+//     el[i].classList.add('dot');
+// }
+
+
+// $(window).scroll(function() {
+//     var scroll = $(window).scrollTop();
+//
+//     if (scroll <= 500) {
+//         $("#appear").addClass("display_none");
+//     } else {
+//         $("#appear").removeClass("display_none");
+//     }
+// });
+
+// event scroll down
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("appear").style.top = "0";
+  } else {
+    document.getElementById("appear").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
 }
-
-
-
 
 
 
